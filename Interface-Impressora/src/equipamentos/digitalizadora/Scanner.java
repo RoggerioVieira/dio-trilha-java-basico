97bea33c663f78162bde0394b9f16a975a18b41f
rogerio vieira <rogeriovieira.a7x@gmail.com>
2024-06-08 13:56:53 -0300

    poo

---
+++ Interface-Impressora/src/equipamentos/digitalizadora/Scanner.java
@@ -0,0 +1,10 @@
+package equipamentos.digitalizadora;
+public class Scanner implements Digitalizadora {
+
+
+    public void Digitalizar() {
+       System.out.println("DIGITALIZANDO");
+       
+    }
+    
+}
---
+++ Interface-Impressora/src/equipamentos/impressora/Laserjat.java
@@ -0,0 +1,8 @@
+package equipamentos.impressora;
+
+public class Laserjat  implements Impressora{
+    public void Imprimir() {
+        System.out.println("IMPRIMINDO LASERJET");
+    }
+    
+}
