dc5f2c676ebbbd5ffdefc80f91c7f0920656d506
RoggerioVieira <rogeriovieira.a7x@gmail.com>
2024-06-17 18:12:14 -0300

    Adicionar arquivos do projeto

---
+++ src/Banco/Agencia.java
@@ -0,0 +1,17 @@
+package Banco;
+import lombok.Data;
+
+@Data
+public class Agencia {
+    public void setNumero(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setNumero(String format) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
